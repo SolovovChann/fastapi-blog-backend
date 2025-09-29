1d9a2f6019ff45234c68d13e74a5898eceda79b3
SolovovChann <solnik2001@gmail.com>
2025-09-29 17:47:07 +0500

    feat: allow authx handle app's errors

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
-from core.config import settings
+from core.config import auth, settings
 
 
 app = FastAPI(debug=settings.DEBUG)
+
+auth.handle_errors(app)
 
 
 if __name__ == "__main__":
