1be03a5ca8db1e3c4635db0aa79ccf6e4e1106e3
SolovovChann <solnik2001@gmail.com>
2025-09-29 17:40:46 +0500

    feat: Settings clas

--- src/core/config.py
+++ src/core/config.py
@@ -1,4 +1,17 @@
 from pathlib import Path
 
+from pydantic_settings import BaseSettings
+
 
 BASE_DIR: Path = Path(__file__).parent.parent.parent
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str = "<DO NOT USE THIS IN PRODUCTION>"
+    # TODO Now debug is True by default. Change to FALSE on commit
+    DEBUG: bool = True
+
+    DB_URL: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite"
+
+
+settings = Settings()
