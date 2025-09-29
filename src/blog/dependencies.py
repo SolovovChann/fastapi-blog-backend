b85c04ca04a7f9fd48a2f41170b4ac661774face
SolovovChann <solnik2001@gmail.com>
2025-09-29 17:51:05 +0500

    feat: get_category_by_slug dependency function

--- src/blog/dependencies.py
+++ src/blog/dependencies.py
@@ -4,7 +4,7 @@ from fastapi import Depends, HTTPException, Path, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from blog import services
-from blog.models import Post
+from blog.models import Category, Post
 from core.database import get_scoped_session
 
 
@@ -19,3 +19,16 @@ async def get_post_by_id(
         raise HTTPException(status.HTTP_404_NOT_FOUND, details)
 
     return post
+
+
+async def get_category_by_slug(
+    category_slug: Annotated[str, Path],
+    session: AsyncSession = Depends(get_scoped_session),
+) -> Category:
+    category = await services.get_category_by_slug(session, category_slug)
+
+    if category is None:
+        details = f"Category with slug={category_slug} is not found"
+        raise HTTPException(status.HTTP_404_NOT_FOUND, details)
+
+    return category
