6e963b4c189824596dc64e1bd6d8f8e64eb283eb
SolovovChann <solnik2001@gmail.com>
2025-09-29 17:53:38 +0500

    feat: create tables at custom fastapi app's lifespan

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
 from core.config import auth, settings
+from core.database import create_tables
 from core.views import router
 
 
-app = FastAPI(debug=settings.DEBUG)
+@asynccontextmanager
+async def fastapi_lifespan(app: FastAPI):
+    await create_tables()
+    yield
+
+
+app = FastAPI(debug=settings.DEBUG, lifespan=fastapi_lifespan)
 app.include_router(router)
 
 auth.handle_errors(app)
