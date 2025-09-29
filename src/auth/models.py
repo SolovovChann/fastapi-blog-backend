190dd89b5f72291f5e87905a06f761d2bdafaafb
SolovovChann <solnik2001@gmail.com>
2025-09-29 17:47:33 +0500

    feat: User ORM model class

---
+++ src/auth/models.py
@@ -0,0 +1,30 @@
+import enum
+
+from sqlalchemy import String, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from core.database import Model
+
+
+class UserRole(enum.Enum):
+    USER = 0
+    ADMIN = 1
+
+
+class User(Model):
+    __tablename__ = "users"
+
+    username: Mapped[str] = mapped_column(String(150), unique=True)
+    full_name: Mapped[str] = mapped_column(String(150), nullable=True)
+    password: Mapped[str] = mapped_column(String(1024), unique=True)
+    role: Mapped[UserRole] = mapped_column(
+        Enum(UserRole),
+        default=UserRole.USER,
+    )
+    posts = relationship("Post", back_populates="author")
+
+    def __str__(self) -> str:
+        return self.username
+
+    def __repr__(self) -> str:
+        return f"User({self.id=}, {self.username=}, {self.full_name=}, {self.role=})"
