f1a3115f7acd9e0d7cb8e07fe0ffc05943f708fe
SolovovChann <solnik2001@gmail.com>
2025-09-29 17:51:18 +0500

    feat: blog pydantic schemas

---
+++ src/blog/schemas.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel
+
+
+class Category(BaseModel):
+    name: str
+    slug: str
+
+
+class CategoryCreate(Category): ...
+
+
+class CategoryUpdate(Category): ...
+
+
+class CategoryUpdatePartial(BaseModel):
+    name: str | None = None
+    slug: str | None = None
+
+
+class BasePost(BaseModel):
+    title: str
+    slug: str
+    content: str
+
+
+class PostCreate(BasePost):
+    categories: list[str]
+
+
+class PostDelete(BaseModel):
+    id: int
+
+
+class Post(BasePost, PostDelete):
+    author: str
+    categories: list[Category]
+
+
+class PostUpdate(BaseModel):
+    title: str
+    content: str
+    categories: list[str]
+
+
+class PostUpdatePartial(BaseModel):
+    title: str | None = None
+    content: str | None = None
+    categories: list[str] | None = None
