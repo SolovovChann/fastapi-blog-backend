fe05b04d264f83e87f90b750dbcbed04159fdce0
SolovovChann <solnik2001@gmail.com>
2025-09-29 17:42:40 +0500

    feat: session_maker

--- src/core/database.py
+++ src/core/database.py
@@ -1,6 +1,12 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from core.config import settings
 
 
 engine = create_async_engine(settings.DB_URL, echo=settings.DEBUG)
+
+session_maker = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+    autoflush=False,
+)
