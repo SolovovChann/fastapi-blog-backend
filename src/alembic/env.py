b4b860f0675494c476effe492d3ecde452579ba1
SolovovChann <solnik2001@gmail.com>
2025-10-01 05:40:06 +0500

    chore: move alembic config to src directory

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,6 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+
 target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
